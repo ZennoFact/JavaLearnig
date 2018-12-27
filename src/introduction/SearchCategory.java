8fdc19bf100aba0d45d625b03ce94c221e3e0e0a
ZennoFact <zenno04.g@gmail.com>
2018-12-27 12:45:59 +0900

    update: Accept user's input.

--- src/introduction/SearchCategory.java
+++ src/introduction/SearchCategory.java
@@ -1,10 +1,29 @@
 package introduction;
 
+import java.util.Locale.Category;
+
 public enum SearchCategory {
-	ID,
-	DEPARTMENT,
-	GENDER,
-	GRADE,
-	KANA,
-	NAME;
+	ID(0),
+	DEPARTMENT(1),
+	GENDER(2),
+	GRADE(3),
+	KANA(4),
+	NAME(5);
+	
+	private int id;
+	
+	private SearchCategory(int id) {
+		this.id = id;
+	}
+	
+	private int getId() {
+		return id;
+	}
+	
+	public static SearchCategory get(int i) {
+		for (SearchCategory category : SearchCategory.values()) {
+			if(category.getId() == i) return category;
+		}
+		return SearchCategory.NAME;
+	}
 }
--- src/introduction/StudentManagementSystem.java
+++ src/introduction/StudentManagementSystem.java
@@ -1,6 +1,7 @@
 package introduction;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.Locale.Category;
 
 public class StudentManagementSystem {
@@ -13,18 +14,33 @@ public class StudentManagementSystem {
 		transfer.placement(Department.MULTIMEDIA_COMPUTING, 3);
 		studentManager.register(transfer);
 		
+		System.out.println("[all students data]");
 		studentManager.showStudentsAll();
+		System.out.println();
 		
-		String[] searchWords = {"ﾗﾉ", "E17", "情報.?学科", "3", "女"};
-		SearchCategory[] categories = {SearchCategory.KANA, SearchCategory.ID, SearchCategory.DEPARTMENT, SearchCategory.GRADE, SearchCategory.GENDER};
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("input > search_word [category_number] | exit (is close)");
+		System.out.println("[Category] 0: ID, 1: 学科, 2: 性別, 3: 学年, 4: カナ, 5: 氏名");
+		System.out.print("> ");
+		String[] inputs = scanner.nextLine().split(" ");
 		
-		for (int i = 0; i < categories.length; i++) {
-			String searchWord = searchWords[i];
-			System.out.println("[search '" + searchWord + "']");
-			List<Student> findStudents = studentManager.search(searchWord, categories[i]);
+		while( ! inputs[0].equals("exit") ) {
+			String word = inputs[0];
+			System.out.println("[search '" + word + "']");
+			int categoryID = 5;
+			if (inputs.length > 1) categoryID = Integer.parseInt(inputs[1]);
+			List<Student> findStudents = studentManager.search(word, SearchCategory.get(categoryID));
 			if (findStudents.size() > 0) studentManager.show(findStudents);
 			else System.out.println("条件に一致する学生は見つかりませんでした。");
+			
+			System.out.println();
+			System.out.println("input > search_word [category_number] | exit (is close)");
+			System.out.println("[Category] 0: ID, 1: 学科, 2: 性別, 3: 学年, 4: カナ, 5: 氏名");
+			System.out.print("> ");
+			inputs = scanner.nextLine().split(" ");
 		}
-		
+
+		System.out.println("bye.");
+		scanner.close();
 	}
 }
